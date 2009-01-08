22f46ee56348d3a7427ee710afd3922260d28ece
Dan Webb <dan@danwebb.net>
2009-01-08 22:53:21 +0000

    got dependency wrong

--- lib/merb_openid.rb
+++ lib/merb_openid.rb
@@ -1,7 +1,7 @@
 if defined?(Merb::Plugins)
   
   Merb::BootLoader.before_app_loads do
-    dependency 'openid', '>= 2.0.0'
+    dependency 'ruby-openid', '>= 2.0.0'
     Merb::Config[:merb_openid] ||= {}
   end
   
